6d349ab2a4386efdb5473fb6facc6e06f05648d3
Quinn Daniel Alfaro <35777556+quinnalfaro@users.noreply.github.com>
2019-09-19 11:29:03 -0700

    Add files via upload

--- BubbleSort.java
+++ BubbleSort.java
@@ -1,7 +1,21 @@
 public class BubbleSort implements SortingAlgorithm {
 
 	public void sort(int [] a){
-			
+		int temp;
+		boolean sorted;
+		for (int i = 0; i < a.length - 1; i++){
+			sorted = true;
+			for (int j = 0; j < a.length - i - 1; j++){
+				if(a[j] > a[j+1]){
+					temp = a[j];
+					a[j] = a[j + 1];
+					a[j + 1] = temp;
+					sorted = false;
+				}
+			}
+			if (sorted == true){
+				break;
+			}
+		}
 	}
-	
 }
